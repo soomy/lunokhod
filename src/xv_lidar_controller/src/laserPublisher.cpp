4766d7105ec44cba3b79bf0ef3c766422576d2d5
Yannick Soom <yannick.soom@gmail.com>
2016-01-09 17:19:16 +0100

    CHANGE: Added check in order that nothing is published when not even one reading of the sensor is received

--- src/xv_lidar_controller/src/laserPublisher.cpp
+++ src/xv_lidar_controller/src/laserPublisher.cpp
@@ -20,33 +20,38 @@ int main(int argc, char** argv){
   while(n.ok()){
 
     int angle; double range; double intensity;
+    bool gotData = false;
 
     while(driver.readNextValues(&angle, &range, &intensity)) {
       ranges[angle] = range;
       intensities[angle] = intensity;
-    }   
-    
-    ros::Time scan_time = ros::Time::now();
-
-    //populate the LaserScan message
-    sensor_msgs::LaserScan scan;
-    scan.header.stamp = scan_time;
-    scan.header.frame_id = "laser_frame";
-    scan.angle_min = -3.1416;
-    scan.angle_max = 3.1416;
-    scan.angle_increment = 6.2832 / num_readings;
-    scan.time_increment = (1 / laser_frequency) / (num_readings);
-    scan.range_min = 0.0;
-    scan.range_max = 100.0;
-
-    scan.ranges.resize(num_readings);
-    scan.intensities.resize(num_readings);
-    for(unsigned int i = 0; i < num_readings; ++i){
-      scan.ranges[i] = ranges[i];
-      scan.intensities[i] = intensities[i];
+      gotData = true;
     }
 
-    scan_pub.publish(scan);
+    if(gotData) {
+ 
+      ros::Time scan_time = ros::Time::now();
+
+      //populate the LaserScan message
+      sensor_msgs::LaserScan scan;
+      scan.header.stamp = scan_time;
+      scan.header.frame_id = "laser_frame";
+      scan.angle_min = -3.1416;
+      scan.angle_max = 3.1416;
+      scan.angle_increment = 6.2832 / num_readings;
+      scan.time_increment = (1 / laser_frequency) / (num_readings);
+      scan.range_min = 0.0;
+      scan.range_max = 100.0;
+
+      scan.ranges.resize(num_readings);
+      scan.intensities.resize(num_readings);
+      for(unsigned int i = 0; i < num_readings; ++i){
+        scan.ranges[i] = ranges[i];
+        scan.intensities[i] = intensities[i];
+      }
+
+      scan_pub.publish(scan);
+    }
   }
   driver.motorOff();
 }
