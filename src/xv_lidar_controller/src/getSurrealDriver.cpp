be36b08ba599f58dae8629cf7b6310e70dead7ec
Yannick Soom <yannick.soom@gmail.com>
2016-01-09 18:11:09 +0100

    FIX: Reverted the previous changes

--- src/xv_lidar_controller/src/getSurrealDriver.cpp
+++ src/xv_lidar_controller/src/getSurrealDriver.cpp
@@ -32,6 +32,9 @@ static const string CMD_MOTOR_ON = "MotorOn\n";
 static const string CMD_HIDE_RAW = "HideRaw";
 static const string CMD_SHOW_RAW = "ShowRaw";
 
+static const string TIME = "Time";
+static const string RPM = "RPM";
+
 GetSurrealDriver::GetSurrealDriver() : serialCon(PORTNAME, BAUDRATE, serial::Timeout::simpleTimeout(TIMEOUT)) {
 }
 GetSurrealDriver::~GetSurrealDriver() {};
@@ -52,29 +55,46 @@ void GetSurrealDriver::motorOff() {
 }
 
 bool GetSurrealDriver::readNextValues(int *angle, double *range, double *intensity) {
-  if(!motorIsOn) motorOn();
-  if(serialCon.isOpen()) {
-    // Read data from serial interface
-    string data = serialCon.readline();
+    if(!motorIsOn) motorOn();
+    if(serialCon.isOpen()) {
+        // Read data from serial interface
+        string data = serialCon.readline();
+        // If data contains either the Word Time or RPM return false
+        std::size_t found = data.find(TIME);
+        if (found!=std::string::npos) return false;
+        found = data.find(RPM);
+        if (found!=std::string::npos) return false;
 
-    // try to read data and if it fails just return false
-    try {
-      // Get the angle value
-      std::size_t found = data.find(":");
-      std::istringstream(data.substr(0, found)) >> *angle;
-      // Get the range value
-      std::size_t start = found+2;
-      found = data.find("(", start);
-      double range_mm = std::atof(data.substr(start, found-1).c_str());
-      *range = range_mm / 1000.0;
-      
-      // Get the intensity value
-      start = found+1;
-      found = data.find(")", start);
-      *intensity = std::atof(data.substr(start, found).c_str());
-    } catch (...) { return false;}
-    
-    return true;
-  }
-  return false;
+        try {
+		// Get the angle value
+		found = data.find(":");
+		std::istringstream(data.substr(0, found)) >> *angle;
+
+		// Get the range value
+		std::size_t start = found+2;
+		found = data.find("(", start);
+		double range_mm = std::atof(data.substr(start, found-1).c_str());
+		*range = range_mm / 1000.0;
+
+		// Get the intensity value
+		start = found+1;
+		found = data.find(")", start);
+		*intensity = std::atof(data.substr(start, found).c_str());
+	} catch (...) { return false;}
+        
+        return true;
+    }
+    return false;
 }
+
+
+
+
+
+
+
+
+
+
+
+
